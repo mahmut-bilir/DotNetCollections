7bd95776557f7a3a9769bc8cdaee253da0ef0aef
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-18 13:05:26 +0300

    Jagged array

--- Program.cs
+++ Program.cs
@@ -12,3 +12,10 @@ for (int i = 0; i < numbers.Length; i++)
 int[,] matrix = { { 1, 8, 7 }, { 2, 4, 6 } };
 Console.WriteLine("Matrix");
 Console.WriteLine(matrix[0, 1]);
+
+int[][] jagged = new int[3][];
+jagged[0] = new int[] { 1, 4, 7 };
+jagged[1] = new int[] { 2, 5, 8 };
+jagged[2] = new int[] { 3, 6, 9 };
+Console.WriteLine("Jagged array");
+Console.WriteLine(jagged[1][1]);
